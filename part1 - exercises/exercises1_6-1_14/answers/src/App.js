d10b893142cf22b217a18804083cca73bf629ac7
Alex Sales <asales2005@gmail.com>
2020-05-30 02:12:30 -0700

    - Answer Exercise 1.7;

--- part1 - exercises/exercises1_6-1_14/answers/src/App.js
+++ part1 - exercises/exercises1_6-1_14/answers/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,18 +6,48 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [moreStats, setMoreStats] = useState({
+    totalResponses: 0,
+    avgFeedback: 0,
+    percentPositive: 0,
+  });
+
+  const updateStats = () => {
+    console.log('stats updated', good, neutral, bad);
+    let numOfFeedback = null;
+    let feedbackAvg = null;
+    let positivePercentage = null;
+
+    numOfFeedback = good + neutral + bad;
+    feedbackAvg = (good * 1 + neutral * 0 + bad * -1) / numOfFeedback;
+    positivePercentage = good / numOfFeedback;
+
+    setMoreStats({
+      ...moreStats,
+      totalResponses: numOfFeedback,
+      avgFeedback: !!feedbackAvg ? feedbackAvg : 'N/A',
+      percentPositive: !!positivePercentage ? positivePercentage : 'N/A',
+    });
+  };
 
   const clickHandler = (evt, feedback) => {
     if (feedback === 'good') {
-      return setGood(good + 1);
+      setGood(good + 1);
+      return updateStats(good + 1, neutral, bad);
     }
     if (feedback === 'neutral') {
-      return setNeutral(neutral + 1);
+      setNeutral(neutral + 1);
+      return updateStats(good, neutral + 1, bad);
     }
     if (feedback === 'bad') {
-      return setBad(bad + 1);
+      setBad(bad + 1);
+      return updateStats(good, neutral, bad + 1);
     }
   };
+
+  useEffect(() => {
+    updateStats();
+  }, [good, neutral, bad]);
 
   return (
     <div className='App'>
@@ -33,9 +63,12 @@ const App = () => {
       <div>
         <h2>Statistics</h2>
         <ul>
-          <li>good {good}</li>
-          <li>neutral {neutral}</li>
-          <li>bad {bad}</li>
+          <li>good: {good}</li>
+          <li>neutral: {neutral}</li>
+          <li>bad: {bad}</li>
+          <li>all: {moreStats.totalResponses}</li>
+          <li>avg feedback: {moreStats.avgFeedback}</li>
+          <li>percent positive: {moreStats.percentPositive}</li>
         </ul>
       </div>
     </div>
