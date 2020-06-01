9ddf5d507f4fa723020d725d1350148f4e51123d
Alex Sales <asales2005@gmail.com>
2020-05-31 21:30:28 -0700

    - Answer Exercise 1.12;

--- part1 - exercises/exercises1_12-1_14/answers/src/App.js
+++ part1 - exercises/exercises1_12-1_14/answers/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [selected, setSelected] = useState(0);
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  ];
+
+  const newQuote = () => {
+    setSelected(Math.floor(Math.random() * 6));
+  };
+
+  useEffect(() => {
+    newQuote();
+  }, []);
+
+  console.log(selected);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='App'>
+        <div>{anecdotes[selected]}</div>
+        <button onClick={newQuote}>Generate Random Quote</button>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
