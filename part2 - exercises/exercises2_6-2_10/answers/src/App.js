06db4a99d3f3767f2178ad26b1ecf27bae441bee
Alex Sales <asales2005@gmail.com>
2020-06-14 11:03:25 -0700

    - Answer Exercise 2.9;

--- part2 - exercises/exercises2_6-2_10/answers/src/App.js
+++ part2 - exercises/exercises2_6-2_10/answers/src/App.js
@@ -3,9 +3,14 @@ import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' },
+    { name: 'John Doe', number: 123 },
+  ]);
+  const [displayedPersons, setDisplayedPersons] = useState(persons);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [searchStr, setSearchStr] = useState('');
 
   const checkDuplicateName = (name) =>
     persons.findIndex((item) => item.name.toLowerCase() === name.toLowerCase());
@@ -19,6 +24,7 @@ const App = () => {
 
     if (checkDuplicateName(nameObject.name) === -1) {
       setPersons(persons.concat(nameObject));
+      setDisplayedPersons(persons.concat(nameObject));
       setNewName('');
       setNewNumber('');
     } else {
@@ -26,17 +32,50 @@ const App = () => {
     }
   };
 
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
+  const filterPersons = (event) => {
+    event.preventDefault();
+
+    if (searchStr.length === 0) {
+      setDisplayedPersons(persons);
+      return false;
+    }
+
+    const indx = persons.findIndex((val, indx) => {
+      const tmpValSplit = val.name.split(' ');
+      const tmpIndx = tmpValSplit.findIndex((tmpVal, tmpIndx) => {
+        return tmpVal.toLowerCase() === searchStr.toLowerCase();
+      });
+
+      return tmpIndx === -1 ? false : true;
+    });
+
+    indx === -1
+      ? setDisplayedPersons([].concat({ name: 'No Match' }))
+      : setDisplayedPersons([].concat(persons[indx]));
   };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearchStrChange = (event) => {
+    setSearchStr(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={filterPersons}>
+        <div>
+          filter shown with{' '}
+          <input value={searchStr} onChange={handleSearchStrChange} />
+        </div>
+      </form>
+      <h2>Add New</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -51,11 +90,13 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         <ul>
-          {persons.map((person) => (
-            <li key={person.name}>
-              {person.name} {person.number}
-            </li>
-          ))}
+          {displayedPersons.map((person) => {
+            return (
+              <li key={person.name}>
+                {person.name} {person.number}
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
