75b18a9c9a668495521d0528e8ea5ba8a34719df
Alex Sales <asales2005@gmail.com>
2020-06-13 17:10:10 -0700

    - Answer Exercise 2.8;

--- part2 - exercises/exercises2_6-2_10/answers/src/App.js
+++ part2 - exercises/exercises2_6-2_10/answers/src/App.js
@@ -5,23 +5,29 @@ import './App.css';
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
-  const checkDuplicateName = (nameObj) =>
-    persons.findIndex(
-      (item) => item.name.toLowerCase() === nameObj.name.toLowerCase()
-    );
+  const checkDuplicateName = (name) =>
+    persons.findIndex((item) => item.name.toLowerCase() === name.toLowerCase());
+
   const addName = (event) => {
     event.preventDefault();
     const nameObject = {
       name: newName,
+      number: newNumber,
     };
 
-    if (checkDuplicateName(nameObject) === -1) {
+    if (checkDuplicateName(nameObject.name) === -1) {
       setPersons(persons.concat(nameObject));
       setNewName('');
+      setNewNumber('');
     } else {
       alert(`${newName} is already added to phonebook`);
     }
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   const handleNameChange = (event) => {
@@ -36,6 +42,9 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type='submit'>add</button>
         </div>
       </form>
@@ -43,7 +52,9 @@ const App = () => {
       <div>
         <ul>
           {persons.map((person) => (
-            <li key={person.name}>{person.name}</li>
+            <li key={person.name}>
+              {person.name} {person.number}
+            </li>
           ))}
         </ul>
       </div>
