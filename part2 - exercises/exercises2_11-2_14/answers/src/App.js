58165d486c893014cc804d7b0584fbcad7f20b66
Alex Sales <asales2005@gmail.com>
2020-06-24 01:16:10 -0700

    - Initial commit for Part 2, Answers to Exercises 2.11 - 2.14; - Answer Exercise 2.11;

--- part2 - exercises/exercises2_11-2_14/answers/src/App.js
+++ part2 - exercises/exercises2_11-2_14/answers/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Note from './components/Person';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newPerson, setNewPerson] = useState('');
+  const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    console.log('effect');
+    axios.get('http://localhost:3001/persons').then((resp) => {
+      console.log('promise fulfilled');
+      setPersons(resp.data);
+    });
+  };
+
+  useEffect(hook, []);
+
+  console.log('render', persons.length, 'persons');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
