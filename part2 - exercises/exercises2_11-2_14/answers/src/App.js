4ce87c294801722d94b9021c25c7cebd4acc32e8
Alex Sales <asales2005@gmail.com>
2020-06-29 22:50:27 -0700

    - Answer Exercise 2.12; - Add basic styling for Exercise 2.12;

--- part2 - exercises/exercises2_11-2_14/answers/src/App.js
+++ part2 - exercises/exercises2_11-2_14/answers/src/App.js
@@ -1,42 +1,123 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Note from './components/Person';
+import Person from './components/Person';
 
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+  // persons
   const [persons, setPersons] = useState([]);
   const [newPerson, setNewPerson] = useState('');
   const [showAll, setShowAll] = useState(true);
 
-  const hook = () => {
-    console.log('effect');
+  // countries
+  const [countries, setCountries] = useState([]);
+
+  // country search str and countriesFiltered
+  const [cSearchStr, setCSearchStr] = useState('');
+  const [cFiltered, setCFiltered] = useState([]);
+
+  const handleCSearchStrChange = (event) => {
+    console.log(event.target.value);
+    setCSearchStr(event.target.value);
+    event.target.value.length === 0
+      ? setCFiltered(countries)
+      : axios
+          .get(`https://restcountries.eu/rest/v2/name/${event.target.value}`)
+          .then((resp) => {
+            console.log('resp, cSearchStr: ', resp, cSearchStr);
+            setCFiltered(resp.data);
+          })
+          .catch((error) => {
+            console.log('error/no match');
+            setCFiltered([]);
+          });
+  };
+
+  const hookPersons = () => {
+    console.log('axios get /persons');
     axios.get('http://localhost:3001/persons').then((resp) => {
-      console.log('promise fulfilled');
+      console.log('persons: ', resp);
       setPersons(resp.data);
     });
   };
 
-  useEffect(hook, []);
+  const hookCountries = () => {
+    console.log('axios get countries');
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then((resp) => {
+        console.log('countries: ', resp);
+        setCountries(resp.data);
+        setCFiltered(resp.data);
+      })
+      .catch(() => {
+        console.log('error/no match');
+        setCountries([]);
+      });
+  };
 
-  console.log('render', persons.length, 'persons');
+  const personsRender = persons.map((person) => {
+    return (
+      <li key={person.id}>
+        {person.name}: {person.number}
+      </li>
+    );
+  });
+
+  const countriesRender = (() => {
+    if (cFiltered.length > 10) {
+      return [<li key={0}>'Please be more specific'</li>];
+    } else if (cFiltered.length === 1) {
+      return [
+        <li key={cFiltered[0].alpha3Code}>
+          <h2>{cFiltered[0].name}</h2>
+          <div>Capital: {cFiltered[0].capital}</div>
+          <div>Population: {cFiltered[0].population}</div>
+          <div>
+            Languages:
+            <ul>
+              {cFiltered[0].languages.map((lang) => (
+                <li key={cFiltered[0].alpha3Code}>{lang.name}</li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            Flag:
+            <span class='flag'>
+              <img src={cFiltered[0].flag} width='200px' />
+            </span>
+          </div>
+        </li>,
+      ];
+    } else if (cFiltered < 1) {
+      return [<li key={0}>'No match found.'</li>];
+    } else {
+      return cFiltered.map((country) => {
+        return <li key={country.alpha3Code}>{country.name}</li>;
+      });
+    }
+  })();
+
+  useEffect(hookPersons, []);
+  useEffect(hookCountries, []);
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <h2>Exercise 2.11</h2>
+      <div>{personsRender}</div>
+
+      <h2>Exercise 2.12</h2>
+      <div>
+        Search:{' '}
+        <input
+          value={cSearchStr}
+          placeholder='Enter country'
+          onChange={handleCSearchStrChange}
+        />
+      </div>
+      <div>{countriesRender}</div>
     </div>
   );
 };
