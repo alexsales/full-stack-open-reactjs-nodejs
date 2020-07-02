6f9e65bc06feb6f2dca9fda00d7bf4aa4384aca2
Alex Sales <asales2005@gmail.com>
2020-07-01 22:41:32 -0700

    - Answer Exercise 2.13;

--- part2 - exercises/exercises2_11-2_14/answers/src/components/Countries.js
+++ part2 - exercises/exercises2_11-2_14/answers/src/components/Countries.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Countries = (props) => {
+  const [selectedCountry, setSelectedCountry] = useState({});
+  const handleShow = (evt, country) => {
+    // console.log('handle show: ', evt.target.getAttribute('id'));
+    evt.preventDefault();
+    setSelectedCountry(country);
+  };
+
   const countriesRender = (() => {
     if (props.cFiltered.length > 10) {
       return [<li key={0}>'Please be more specific'</li>];
@@ -14,7 +21,7 @@ const Countries = (props) => {
             Languages:{' '}
             <ul>
               {props.cFiltered[0].languages.map((lang) => (
-                <li key={props.cFiltered[0].alpha3Code}>{lang.name}</li>
+                <li key={lang.iso639_1}>{lang.name}</li>
               ))}
             </ul>
           </div>
@@ -34,11 +41,46 @@ const Countries = (props) => {
       return [<li key={0}>'No match found.'</li>];
     } else {
       return props.cFiltered.map((country) => {
-        return <li key={country.alpha3Code}>{country.name}</li>;
+        return (
+          <li key={country.alpha3Code}>
+            {country.name}{' '}
+            <input
+              id={country.alpha3Code}
+              type='button'
+              value='show'
+              onClick={(evt) => handleShow(evt, country)}
+            />
+          </li>
+        );
       });
     }
   })();
-  return <div>{countriesRender}</div>;
+  return (
+    <>
+      <div>{countriesRender}</div>
+      <div>
+        <h2>{selectedCountry.name}</h2>
+        <div>Capital: {selectedCountry.capital}</div>
+        <div>Population: {selectedCountry.population}</div>
+        <div>
+          Languages:{' '}
+          <ul>
+            {selectedCountry.languages !== undefined
+              ? selectedCountry.languages.map((lang) => (
+                  <li key={lang.iso639_1}>{lang.name}</li>
+                ))
+              : null}
+          </ul>
+        </div>
+        <div>
+          Flag:{' '}
+          <span className='flag'>
+            <img src={selectedCountry.flag} width='200px' alt='country flag' />
+          </span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Countries;
